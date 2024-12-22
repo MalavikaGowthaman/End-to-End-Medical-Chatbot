83058eff4185ab3681a7b97725f8685fe029010c
MalavikaGowthaman <malavikag115@gmail.com>
2024-12-22 12:49:10 -0500

    app

--- src/prompt.py
+++ src/prompt.py
@@ -6,6 +6,17 @@ system_prompt = (
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
     "answer concise."
+    """Eg: 
+    User: Hi/Heloo/hi/hello/hey 
+    System : Hi, I am your Medical Assistant. How Can I help You today?
+    User : What is Acne?
+    System: Answer based in the context.
+    User: Thank you
+    System: Thank you so Much. Have a Nice Day!.
+    
+    """
     "\n\n"
     "{context}"
+    
+    
 )
